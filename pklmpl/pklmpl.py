9532cd408364e0029372d5e29056b142fd5e26e3
Sebastien Roche <sroche@g.harvard.edu>
2023-02-10 12:02:26 -0500

    added option to save the pickled figure as a png

--- pklmpl/pklmpl.py
+++ pklmpl/pklmpl.py
@@ -5,7 +5,7 @@ from matplotlib.figure import Figure
 from matplotlib import pylab
 
 
-def pickle_mpl_figure(fig: Figure, path: str):
+def pickle_mpl_figure(fig: Figure, path: str) -> None:
     """
     Save an mpl figure using pickle
     """
@@ -13,7 +13,7 @@ def pickle_mpl_figure(fig: Figure, path: str):
         pickle.dump(fig, outfile)
 
 
-def open_pickled_mpl_figure(path: str):
+def open_pickled_mpl_figure(path: str) -> None:
     """
     Open a pickled matplotlib figure
     path (str): full path to the pickle file
@@ -24,15 +24,46 @@ def open_pickled_mpl_figure(path: str):
     pylab.show(block=True)
 
 
+def pickle_to_png(path: str, outfile: str = "") -> None:
+    """
+    Open a pickled matplotlib figure and save it as a .png
+    path (str): full path to the pickle file
+    outfile (str): full path to the png file, if left as empty string, just use "path" with a png extension
+    """
+
+    with open(path, "rb") as infile:
+        fig = pylab.figure(pickle.load(infile))
+
+    if not outfile:
+        outfile = path.replace(".pickle", ".png")
+    fig.savefig(outfile)
+
+
 def main():
     parser = argparse.ArgumentParser(description="Open a pickled matplotlib figure")
     parser.add_argument("path", help="full path to the pickle file")
+    parser.add_argument(
+        "-m",
+        "mode",
+        choices=["open", "png"],
+        default="open",
+        help="one of [open,png], use to either open up the figure or save it to a png",
+    )
+    parser.add_argument(
+        "-o",
+        "--outfile",
+        default="",
+        help="Only used with the png mode, full path to the file of the output png file, if not given it will just replace '.pickle' with '.png' in the path argument",
+    )
     args = parser.parse_args()
 
     if not os.path.exists(args.path):
         raise Exception(f"Wong path: {args.path}")
 
-    open_pickled_mpl_figure(args.path)
+    if args.mode == "open":
+        open_pickled_mpl_figure(args.path)
+    elif args.mode == "png":
+        pickle_to_png(args.path, args.outfile)
 
 
 if __name__ == "__main__":
